cf6fc56d00b1b6daefad36c7e1752ae401c8dbe3
Prateek Kaushik <kaushikprateek11@gmail.com>
2020-08-20 16:15:51 +0530

    minor

--- src/Models/UserModel.ts
+++ src/Models/UserModel.ts
@@ -5,11 +5,9 @@ class User extends Model{
     super([
       { property: 'firstname', type: 'string', required: true },
       { property: 'lastname', type: 'string', required: true },
-      { property: 'email', type: 'email', required: true },
+      { property: 'email', type: 'email', required: true, unique: true },
       { property: 'password', type: 'string', required: true }
-    ],
-    'User'
-    )
+    ], 'User')
   }
   //own function here...
 }
