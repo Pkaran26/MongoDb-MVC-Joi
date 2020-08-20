fe6c6bfcbf3a669f6fd249ab8bc56151e9e0df31
Prateek Kaushik <kaushikprateek11@gmail.com>
2020-08-20 11:58:09 +0530

    app as module

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-import * as Koa from "koa";
-// import * as Router from "koa-router";
-import * as logger from "koa-logger";
-import * as json from "koa-json";
-import * as bodyParser from "koa-bodyparser";
-import { requestInterCeptor } from "./Core/RequestMiddleware/Request"
-
+import app from './Core/index'
 import userRouter from './Routes/UserRoutes'
 
-const app = new Koa();
 const PORT = 3000;
-
-/** Middlewares */
-app.use(json());
-app.use(logger());
-app.use(bodyParser());
-app.use(requestInterCeptor);
 
 app.listen(PORT, ()=>{
   console.log('server running...')
